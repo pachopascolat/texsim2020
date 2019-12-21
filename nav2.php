8be1c8660702ab3bec0d83ec53f30b095d8ae44e
pachopascolat <patriciopascolat@gmail.com>
2019-12-21 19:42:02 -0300

    "terminado nav2?"

--- nav2.php
+++ nav2.php
@@ -1,24 +1,38 @@
+<h1>hola Mundo</h1>
 <div class="nav2">
- <img src="./imgHeader2/faja-01.jpg" class="nav2-img w-100">
- <nav class="justify-content-end navbar">
-     <ul class="navbar-nav">
-         <li class="nav-item">
-             <a href="#">Novedades</a>
-         </li>
-         <li class="nav-item">
-             <a href="#">Mis Consultas</a>
-         </li>
-         <li class="nav-item">
-             <a href="#">Ayuda</a>
-         </li>
-         <li class="nav-item">
-             <a href="#">Sucursales</a>
-         </li>
-         <li class="nav-item">
-             <a href="#">
-                 <img src="">
-             </a>
-         </li>
-     </ul>
- </nav>
+
+    <img src="./imgHeader2/faja-02.jpg" class="nav2-img">
+    <div class="absolute-div w-100">
+        <div class="container">
+            <nav class="navbar navbar-expand justify-content-end">
+                <div>
+                    <ul class="navbar-nav mr-auto ">
+                        <li class="nav-item">
+                            <a class="" href="#">Novedades</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="" href="#">Mis Consultas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="" href="#">Ayuda</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="" href="#">Sucursales</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="" href="#">
+                                <img src="">
+                            </a>
+                        </li>
+                        <li class="nav-item navbar-icon-link">
+                            <div class="text-white" data-toggle="modal" data-target="#login-modal">
+                                <i style="" class="fas fa-lock svg-icon "></i>
+                                <span class="d-xs-block d-sm-none">Login</span>
+                            </div>
+                        </div>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </div>
 </div>

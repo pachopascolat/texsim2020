df1186d5b2b254afaf3e6add85e0ea728f0f5820
pachopascolat <patriciopascolat@gmail.com>
2019-12-21 15:55:50 -0300

    esqueleto y algunas imagenes

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include_once('header.php') ?>
+<?php include_once('nav1.php') ?>
+<?php include_once('nav2.php') ?>
+    
+    </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+
+
+<?php include_once('footer.php') ?>

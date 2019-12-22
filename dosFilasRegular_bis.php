7790209abd4ae174a477112b72213dbb78870af2
pachopascolat <patriciopascolat@gmail.com>
2019-12-22 14:19:14 -0300

    cuadricula infantil

---
+++ dosFilasRegular_bis.php
@@ -0,0 +1,68 @@
+<div class="dos-filas-regular">
+    <div class="container">
+        <h3>Lo que se viene en Infantil</h3>
+
+        <div class="cuadricula-regular">
+            <div class="row">
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_040.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_041.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_042.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_043.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_044.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_045.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_046.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_047.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_048.jpg">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_049.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_050.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_051.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_052.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_039.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_038.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_037.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_036.jpg">
+                </div>
+                <div class="col">
+                    <img class="disenio-img img-fluid" src="designs/fibrana/fibrana_035.jpg">
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -4,8 +4,10 @@
 
     
     <?php require_once('dosFilasRegular.php') ?>
+<br>
     <?php require_once('./dosFilasPrincipal.php') ?>
-    
+<br>  
+  <?php require_once('./dosFilasRegular_bis.php') ?>
 
 
 
